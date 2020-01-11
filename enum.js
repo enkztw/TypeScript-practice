0a4b5d49129290cb855e50abaf49e48d0adb89d9
KuznetsovMA <jztenk@gmail.com>
2020-01-11 16:35:26 +0300

    enums

---
+++ enum.js
@@ -0,0 +1,10 @@
+var Membership;
+(function (Membership) {
+    Membership[Membership["Simple"] = 0] = "Simple";
+    Membership[Membership["Standart"] = 1] = "Standart";
+    Membership[Membership["Premium"] = 2] = "Premium";
+})(Membership || (Membership = {}));
+var membership = Membership.Standart;
+// Reverse enum
+var membershipReverse = Membership[2];
+console.log(membershipReverse);
---
+++ enum.ts
@@ -0,0 +1,19 @@
+
+enum Membership {
+    Simple,
+    Standart,
+    Premium
+}
+
+const membership = Membership.Standart;
+// Reverse enum
+const membershipReverse = Membership[2];
+
+enum SocialMedia {
+    VK = 'VK',
+    FB = 'FACEBOOK',
+    INST = 'INSTAGRAM'
+}
+
+const social = SocialMedia.INST;
+
